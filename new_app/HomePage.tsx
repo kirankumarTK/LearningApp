b02d903f196a111ac0b0364a5e40cefff677db85
kirankumar.tk <kirankumar.tk@ivymobility.com>
2024-08-01 09:48:23 +0530

    Intial setup

---
+++ new_app/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+class HomePage extends React.PureComponent {
+    state = {  }
+    render() {
+        return (
+            <Text>Hello World</Text>
+        );
+    }
+}
+
+export default HomePage;
